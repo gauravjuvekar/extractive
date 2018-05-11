c1272ef81a1f559fea56325901ed9bec17674f6c
Gaurav Juvekar <gauravjuvekar@gmail.com>
2018-05-11 17:23:56 +0530

    Fix evalute script

--- evaluate.sh
+++ evaluate.sh
@@ -6,25 +6,40 @@ cp -r "$OUT_DIR" output_tmp
 
 OP_DIR="output_tmp/opinosis"
 
-for folder in "$OP_DIR/*"
+for folder in $OP_DIR/*
 do
-    for file in "$folder/*"
+    THIS_DIR=$(pwd)
+    cd "$folder"
+    for file in ./*.txt.data*
     do
         echo $file
         rename -E 's/_//g' "$file"
-        rename -E 's/\.txt\.data(K[1-9]+)-(euclidean|cosine)/_'"$folder"'$2\.$1/' "$file"
     done
+    for file in ./*.txt.data*
+    do
+        rename -E 's/\.txt\.data(K[1-9]+)-(euclidean|cosine)/_'`basename $folder`'$2\.$1/' "$file"
+    done
+    cd "$THIS_DIR"
 done
+
 
 OP_DIR="output_tmp/cmplg"
 
-for folder in "$OP_DIR/*"
+for folder in $OP_DIR/*
 do
-    for file in "$folder/*"
+    THIS_DIR=$(pwd)
+    cd "$folder"
+    for file in ./*.txt.data*
     do
         echo $file
         rename -E 's/_//g' "$file"
-        rename -E 's/body\.txt(K[1-9]+)-(euclidean|cosine)/_'"$folder"'$2\.$1/' "$file"
     done
+    for file in ./*.txt.data*
+    do
+        rename -E 's/body\.txt(K[1-9]+)-(euclidean|cosine)/_'`basename $folder`'$2\.$1/' "$file"
+    done
+    cd "$THIS_DIR"
 done
+
+
 
