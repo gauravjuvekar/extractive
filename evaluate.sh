a009a72e1fc73c1d9afee8de976346687ca9b9d3
Gaurav Juvekar <gauravjuvekar@gmail.com>
2018-05-11 18:51:30 +0530

    UPdate main evaluate

--- evaluate.sh
+++ evaluate.sh
@@ -43,4 +43,20 @@ do
 done
 
 
+OPINOSIS_DIR=$(realpath ../../data/opinosis/gold)
+CMPLG_DIR=$(realpath ../../data/cmplg-xml/gold)
 
+OP_SIF=$(realpath ./output_tmp/opinosis/sif)
+OP_S2V=$(realpath ./output_tmp/opinosis/s2v)
+OP_SNP=$(realpath ./output_tmp/opinosis/sifnopcr)
+CP_SIF=$(realpath ./output_tmp/cmplg/sif)
+CP_S2V=$(realpath ./output_tmp/cmplg/s2v)
+CP_SNP=$(realpath ./output_tmp/cmplg/sifnopcr)
+
+cd "./eval_dir"
+./init.sh OP_SIF "$OPINOSIS_DIR" "$OP_SIF"
+./init.sh OP_S2V "$OPINOSIS_DIR" "$OP_S2V"
+./init.sh OP_SNP "$OPINOSIS_DIR" "$OP_SNP"
+./init.sh CP_SIF "$CMPLG_DIR" "$CP_SIF"
+./init.sh CP_S2V "$CMPLG_DIR" "$CP_S2V"
+./init.sh CP_SNP "$CMPLG_DIR" "$CP_SNP"
